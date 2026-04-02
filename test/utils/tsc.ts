cb435657783213b50829beaea919400c3e3f97be
John Wright <john.wright@mailmetromedia.co.uk>
2026-04-02 14:40:31 +0100

    fix: update typecheck function to use dynamic tsc path and enhance options

--- test/utils/tsc.ts
+++ test/utils/tsc.ts
@@ -1,10 +1,18 @@
 import { promisify } from "node:util";
 import { execFile as execFileCb } from "node:child_process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const execFile = promisify(execFileCb);
+const rootDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../..");
+const tscPath = path.join(rootDir, "node_modules", ".bin", "tsc");
 
 export async function typecheck(pathToIndex: string) {
-    await execFile("tsc", [pathToIndex, "--noEmit", "--esModuleInterop"], {
-        env: process.env,
-    });
+    await execFile(
+        tscPath,
+        [pathToIndex, "--noEmit", "--esModuleInterop", "--ignoreConfig", "--types", "node", "--skipLibCheck"],
+        {
+            env: process.env,
+        }
+    );
 }
